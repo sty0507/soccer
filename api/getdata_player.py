bc2331b15eede61c8272863309e654c7b4950a73
sty0507 <styoung0507@gmail.com>
2022-05-27 21:41:38 +0900

    특정 팀에 소속된 선수 구하기 -> 실패

--- api/getdata_player.py
+++ api/getdata_player.py
@@ -1,10 +1,30 @@
 import http.client
 import json
-
+from threading import currentThread
 import pandas as pd
 
 connection = http.client.HTTPConnection('api.football-data.org')
 headers = { 'X-Auth-Token' : 'a5138360d35743f1961282ccb58f088f' }
-connection.request('GET', '/v2/players/40', None, headers )
-response = json.loads(connection.getresponse().read().decode())
-print(response)
+
+numlist = []
+
+for i in range(1000):
+    r = '/v4/persons/'
+    r = r + str(i)
+    connection.request('GET', r, None, headers)
+    response = json.loads(connection.getresponse().read().decode())
+    if "currentTeam" in response:
+        numlist.append(i)
+
+for j in range(len(numlist)):
+    r = '/v4/persons/'
+    r = r + str(numlist[j])
+    connection.request('GET', r, None, headers)
+    response = json.loads(connection.getresponse().read().decode())
+    currentTeam = response.get('currentTeam')
+    Teamname = currentTeam.get('name')
+    if Teamname == "Manchester United FC":
+        print(response.get('name'))
+    else:
+        continue
+    
